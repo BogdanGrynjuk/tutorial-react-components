79cd3a94aa7d75c8f3a3f69297024f6fed88aefe
BogdanGrynjuk <bgrynjuk@ukr.net>
2024-08-18 16:58:13 +0300

    adds social links

--- src/layout/social-sidebar/SocialSidebar .jsx
+++ src/layout/social-sidebar/SocialSidebar .jsx
@@ -1,19 +1,42 @@
 import React from 'react';
 import css from './SocialSidebar.module.css';
+import githubIcon from '../../images/social-icon-github.svg';
+import linkedinIcon from '../../images/social-icon-linkedIn.svg';
+import telegramIcon from '../../images/social-icon-telegram.svg';
 
 const SocialSidebar = () => (
   <aside className={css['sidebar-wrapper']}>
-    <ul>
+    <ul className={css['social-icons']}>
       <li>
         <a
-          className={css.link}
-          href="https://github.com/BogdanGrynjuk/tutorial-react-components"
+          className={css['social-link']}
+          href="https://github.com/BogdanGrynjuk/"
+          target="_blank"
+          rel="noreferrer"
         >
-          1
+          <img src={githubIcon} alt="GitHub" width={25} height={25} />
         </a>
       </li>
-      <li>2</li>
-      <li>3</li>
+      <li>
+        <a
+          className={css['social-link']}
+          href="http://linkedin.com/in/bogdan-grynjuk-7024a710a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={linkedinIcon} alt="LinkedIn" width={30} height={30} />
+        </a>
+      </li>
+      <li>
+        <a
+          className={css['social-link']}
+          href="https://t.me/bogdan_grynjuk"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={telegramIcon} alt="Telegram" width={25} height={25} />
+        </a>
+      </li>
     </ul>
 
     <div className={css['vertical-text']}>
